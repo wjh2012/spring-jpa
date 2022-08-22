957af1afe296b6a977fb1467f8d4df7616257059
wjh2012 <wjh2012@naver.com>
2022-08-22 23:33:36 +0900

    스프링 컨테이너/빈 부모타입으로 조회하기

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -23,7 +23,7 @@ public class ApplicationContextSameBeanFindTest {
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류 발생")
     void findBeanByTypeDuplicate(){
         assertThrows(NoUniqueBeanDefinitionException.class,
-                () ->ac.getBean(MemberRepository.class));
+                () -> ac.getBean(MemberRepository.class));
     }
 
     @Test
