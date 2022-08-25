f0289a7d4c25523f82c01a7c4ac97d9aa6b5d232
wjh2012 <wjh2012@naver.com>
2022-08-25 22:18:59 +0900

    2022-08-25 스프링 컴포넌트 스캔2

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -18,5 +21,8 @@ import org.springframework.context.annotation.FilterType;
         // 범위를 지정하지 않으면 이 파일이 포함된 패키지 하위 컴포넌트들을 스캔함
 )
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
