7660f9b8c41c8c64c3e68a5405ed7e7819f2878e
wjh2012 <wjh2012@naver.com>
2022-09-08 22:10:51 +0900

    2022-09-08 여러 개의 스프링 빈을 조회할 때

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,19 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // 의존성 문제 발생
@@ -19,8 +23,9 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;// = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy;// = new RateDiscountPolicy();
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+// 롬복 @RequiredArgsConstructor 에서 생성자를 자동으로 만들어주기 때문에 없어도 됨
+    @Autowired// 생성자가 1개이기 때문에 생략 가능
+    public OrderServiceImpl(MemberRepository memberRepository, /*@Qualifier("fixDiscountPolicy")*/ @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -29,10 +34,12 @@ public class OrderServiceImpl implements OrderService{
 //    @Autowired private MemberRepository memberRepository;
 //    @Autowired private DiscountPolicy discountPolicy;
 
-//   public void setMemberRepository(MemberRepository memberRepository) {
+    // setter 주입
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
 //        this.memberRepository = memberRepository;
 //    }
-//
+//    @Autowired
 //    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
 //        this.discountPolicy = discountPolicy;
 //    }
