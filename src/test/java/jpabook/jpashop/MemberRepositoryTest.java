ff800b12ea74b06e4dc1be53e19f5cfb2ec17a99
wjh2012 <wjh2012@naver.com>
2022-09-20 23:32:58 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member foundMember = memberRepository.find(savedId);
+
+        //then
+        assertEquals(foundMember.getId(), member.getId());
+        assertEquals(foundMember.getUsername(), member.getUsername());
+        assertEquals(foundMember, member);
+    }
+}
