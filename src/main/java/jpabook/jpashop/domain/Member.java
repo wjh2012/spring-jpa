c74c860bfac0589672c466bed0681f443b26c901
wjh2012 <wjh2012@naver.com>
2022-09-23 23:43:27 +0900

    2022-09-23 도메인 설계

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // (거울, 읽기전용) order 테이블의 "member" 필드에 의해서 매핑됨
+    private List<Order> orders = new ArrayList<>();
+}
+
